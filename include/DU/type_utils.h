ac9e00f16cdd884f4889239e4b2d1566f317415e
Dmitrii Ubskii <ubskydm@gmail.com>
2022-02-04 18:17:35 +0300

    ConstnessAs<T, U> and FirstOfPack<...>

--- include/DU/type_utils.h
+++ include/DU/type_utils.h
@@ -16,6 +16,30 @@ constexpr bool allSame<T, U> = std::is_same_v<T, U>;
 
 template <typename T, typename U, typename... Rest>
 constexpr bool allSame<T, U, Rest...> = allSame<T, U> && allSame<U, Rest...>;
+
+template <typename To, typename From>
+struct ConstnessAs
+{
+	using Type = std::remove_const_t<To>;
+};
+
+template <typename To, typename From>
+struct ConstnessAs<To, From const>
+{
+	using Type = std::add_const_t<To>;
+};
+
+template <typename To, typename From>
+using ConstnessAsT = typename ConstnessAs<To, From>::Type;
+
+template <typename T, typename... Rest>
+struct FirstOfPack
+{
+	using Type = T;
+};
+
+template <typename... Ts>
+using FirstOfPackT = typename FirstOfPack<Ts...>::Type;
 }
 
 #endif // DU_TYPE_UTILS_H_
